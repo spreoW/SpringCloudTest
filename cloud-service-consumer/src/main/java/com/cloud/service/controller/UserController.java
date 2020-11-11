69fa1e93f852295a150d946b201626d5e92f3e98
wq <1563904374@qq.com>
2020-11-11 08:33:09 +0800

    consumer服务消费者的修改

--- cloud-service-consumer/src/main/java/com/cloud/service/controller/UserController.java
+++ cloud-service-consumer/src/main/java/com/cloud/service/controller/UserController.java
@@ -2,12 +2,16 @@ package com.cloud.service.controller;
 
 import com.cloud.service.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author w
@@ -20,9 +24,17 @@ public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping
     @ResponseBody
     public User queryUserById(@RequestParam("id") Long id){
-        return restTemplate.getForObject("http://localhost:8081/user/"+id,User.class);
+        // 根据服务名称，获取服务实例。有可能是集群，所以是service实例集合
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
+        ServiceInstance instance = instances.get(0);
+        String baseUrl = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/" + id;
+        //return restTemplate.getForObject("http://localhost:8081/user/"+id,User.class);
+        return this.restTemplate.getForObject(baseUrl, User.class);
     }
 }
