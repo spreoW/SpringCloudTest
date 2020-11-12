18a2403b8017714b901f8648a49c03732b6b9aa3
wq <1563904374@qq.com>
2020-11-12 08:37:52 +0800

    add zuul

---
+++ cloud-zuul/src/test/java/com/cloud/zuul/CloudZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
